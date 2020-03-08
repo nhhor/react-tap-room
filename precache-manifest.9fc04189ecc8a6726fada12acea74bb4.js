cc7d27cc90b082688dcfbced408278ec6f33fbbc
nhhor <nhhor@post.com>
2020-03-08 15:40:34 -0700

    Updates

---
+++ precache-manifest.9fc04189ecc8a6726fada12acea74bb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c094d296102e2235165ce6caa061e339",
+    "url": "/react-tap-room/index.html"
+  },
+  {
+    "revision": "50e429df8133560b38ce",
+    "url": "/react-tap-room/static/css/main.2aae2077.chunk.css"
+  },
+  {
+    "revision": "2981e5f9a57618766a50",
+    "url": "/react-tap-room/static/js/2.7d82e44c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tap-room/static/js/2.7d82e44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e429df8133560b38ce",
+    "url": "/react-tap-room/static/js/main.29b65eae.chunk.js"
+  },
+  {
+    "revision": "3d03159aa3abca06da76",
+    "url": "/react-tap-room/static/js/runtime-main.41e5ae24.js"
+  },
+  {
+    "revision": "f65ba043a2b94f004a4b3c77d113ae47",
+    "url": "/react-tap-room/static/media/InfoIcon.f65ba043.svg"
+  },
+  {
+    "revision": "5fa890b59420099542cdeeb84417e035",
+    "url": "/react-tap-room/static/media/KegIcon.5fa890b5.svg"
+  }
+]);
